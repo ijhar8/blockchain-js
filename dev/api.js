b1faaec46d4043bbeabdb4dc0fa98b1416ea234e
ijhar8 <ijharansari8@gmail.com>
2019-03-29 00:03:59 +0530

    api base

--- dev/api.js
+++ dev/api.js
@@ -1,19 +1,30 @@
 var express = require('express')
 var app = express()
-var bodyParser=require('body-parser')
+var bodyParser = require('body-parser')
+var Blockchain = require('./blockchain')
+
+bitcoin=new Blockchain();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}))
  
 app.get('/blockchain', function (req, res) {
 
+    res.send(bitcoin)
+
 })
 
 app.post('/transaction', function (req, res) {
 
+    const blockIndex=bitcoin.createNewTransaction(req.body.amount,req.body.sender,req.body.recipient)
+
+    res.json({blockIndex:`you block added at ${blockIndex}`})
+
 })
 
 app.get('/mine', function (req, res) {
+
+const nonce=bitcoin.proofOfWork();
 
 })
 
